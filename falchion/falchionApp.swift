2d402aaad7d0617f01dab4f9e7ffeec47201e151
any-one-but <any-one-but@users.noreply.github.com>
2026-02-11 07:08:41 -0500

    Initial Commit

---
+++ falchion/falchionApp.swift
@@ -0,0 +1,32 @@
+//
+//  falchionApp.swift
+//  falchion
+//
+//  Created by Jo Walsh on 2/11/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct falchionApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
